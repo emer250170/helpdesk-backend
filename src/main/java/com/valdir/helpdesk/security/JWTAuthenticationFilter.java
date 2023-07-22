250d62121d714af4fb9d3b35e906b5b464d0b0ac
emer250170 <emerson.oliveira@gmail.com>
2023-07-22 20:07:14 -0300

    Ajuste no arquivo JWTAuthenticationFilter.

--- src/main/java/com/valdir/helpdesk/security/JWTAuthenticationFilter.java
+++ src/main/java/com/valdir/helpdesk/security/JWTAuthenticationFilter.java
@@ -51,10 +51,10 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 	
 		String username = ((UserSS) auth.getPrincipal()).getUsername();
 		String token = jwtUtil.generateToken(username);
-		response.setHeader("Access-Control-Allow-Origin", "*");
-        	response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
-        	response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, enctype, Location");
-        	response.setHeader("Authorization", "Bearer " + token);
+		res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+        res.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, enctype, Location");
+        res.setHeader("Authorization", "Bearer " + token);
 	}
 	
 	@Override
